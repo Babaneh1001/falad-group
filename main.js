02aebfd46c618b66615c57f97a823deac4caab8e
Babaneh1001 <travisneh1996@gmail.com>
2023-10-02 14:33:58 +0100

    fixed the hamburger menu

--- main.js
+++ main.js
@@ -1,24 +1,14 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const openHam = document.querySelector('.open-hamburger');
+const closeHam = document.querySelector('.navigation-for-hamburger');
+const showDiv = document.querySelector('.hamburger-menu');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+openHam.addEventListener("click", () => {
+  openHam.classList.add('hidden');
+  showDiv.classList.remove('hidden');
+});
+
+closeHam.addEventListener("click", () => {
+  showDiv.classList.add('hidden');
+  openHam.classList.remove('hidden');
+});
